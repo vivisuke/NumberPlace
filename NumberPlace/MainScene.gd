ce33d822efe266ce9c43b365753b3e8a2b66fad5
vivisuke <ntsuda@master.email.ne.jp>
2021-11-21 07:50:26 +0900

    0023 ◎ C 11/21 11/21 0.001│ToDo│移動処理を関数化

--- NumberPlace/MainScene.gd
+++ NumberPlace/MainScene.gd
@@ -1,16 +1,110 @@
 extends Node2D
 
+const N_VERT = 9
+const N_HORZ = 9
+const N_CELLS = N_HORZ * N_VERT
+const CELL_WIDTH = 53
+const BIT_1 = 1
+const BIT_2 = 1<<1
+const BIT_3 = 1<<2
+const BIT_4 = 1<<3
+const BIT_5 = 1<<4
+const BIT_6 = 1<<5
+const BIT_7 = 1<<6
+const BIT_8 = 1<<7
+const BIT_9 = 1<<8
+const ALL_BITS = (1<<N_HORZ) - 1
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var cell_bit = []			# 各セル数値（0 | BIT_1 | BIT_2 | ... | BIT_9）
+var column_used = []		# 各カラムの使用済みビット
+var box_used = []			# 各3x3ブロックの使用済みビット
+#var line_used_bits
+var clue_labels = []			# 手がかり数字用ラベル配列
+var ClueLabel = load("res://ClueLabel.tscn")
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	cell_bit.resize(N_CELLS)
+	column_used.resize(N_HORZ)
+	box_used.resize(N_HORZ)
+	for i in range(box_used.size()): box_used[i] = 0
+	for y in range(N_VERT):
+		for x in range(N_HORZ):
+			var label = ClueLabel.instance()
+			clue_labels.push_back(label)
+			label.rect_position = Vector2(x*CELL_WIDTH, y*CELL_WIDTH+2)
+			label.text = String((x+y)%9 + 1)
+			$Board.add_child(label)
+	gen_ans()
+	pass
+func bit_to_num(b):
+	var mask = 1
+	for i in range(N_HORZ):
+		if (b & mask) != 0: return i + 1
+		mask <<= 1
+	return 0
+func print_cells():
+	var ix = 0
+	for y in range(N_VERT):
+		var lst = []
+		for x in range(N_HORZ):
+			lst.push_back(bit_to_num(cell_bit[ix]))
+			ix += 1
+		print(lst)
+	print("")
+func print_box_used():
+	var txt = ""
+	for i in range(box_used.size()):
+		txt += "%03x " % box_used[i]
+	print(txt)
+func update_cell_labels():
+	var ix = 0
+	for y in range(N_VERT):
+		for x in range(N_HORZ):
+			clue_labels[ix].text = String(bit_to_num(cell_bit[ix]))
+			ix += 1
+# cell_bit[ix] 数字を入れる
+# return: true for 解答生成成功
+func gen_ans_sub(ix : int, line_used):
+	#print_cells()
+	#print_box_used()
+	var x : int = ix % N_HORZ
+	if x == 0: line_used = 0
+	var x3 = x / 3
+	var y3 = ix / (N_HORZ*3)
+	var bix = y3 * 3 + x3
+	var used = line_used | column_used[x] | box_used[bix]
+	if used == ALL_BITS: return false		# 全数字が使用済み
+	var lst = []
+	var mask = BIT_1
+	for i in range(N_HORZ):
+		if (used & mask) == 0: lst.push_back(mask)		# 数字未使用の場合
+		mask <<= 1
+	if ix == N_CELLS - 1:
+		cell_bit[ix] = lst[0]
+		return true
+	if lst.size() > 1: lst.shuffle()
+	for i in range(lst.size()):
+		cell_bit[ix] = lst[i]
+		column_used[x] |= lst[i]
+		box_used[bix] |= lst[i]
+		if gen_ans_sub(ix+1, line_used | lst[i]): return true
+		column_used[x] &= ~lst[i]
+		box_used[bix] &= ~lst[i]
+	cell_bit[ix] = 0
+	return false;
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func gen_ans():		# 解答生成
+	for i in range(cell_bit.size()): cell_bit[i] = 0
+	var t = []
+	for i in range(N_HORZ): t.push_back(1<<i)
+	t.shuffle()
+	for i in range(N_HORZ):
+		cell_bit[i] = t[i]
+		column_used[i] = t[i]
+		box_used[i/3] |= t[i]
+	#print(cell_bit)
+	gen_ans_sub(N_HORZ, 0)
+	print_cells()
+	update_cell_labels()
+	pass
