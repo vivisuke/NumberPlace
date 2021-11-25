e951e05fb0efcbdf5eb5c9fb3c13b54c1e1048a2
vivisuke <ntsuda@master.email.ne.jp>
2021-11-25 11:39:03 +0900

    0032 ◎ C 11/25 11/25 0.001│ToDo│盤面：グリッドを _draw() で描画 0033 ◎ C 11/25 11/25 0.001│ToDo│盤面背景：ColorRect に変更し、市松模様を描画

---
+++ NumberPlace/Board.gd
@@ -0,0 +1,20 @@
+extends ColorRect
+
+const N_VERT = 9
+const N_HORZ = 9
+const CELL_WIDTH = 53
+const BOX_WIDTH = CELL_WIDTH * 3
+const BOARD_WIDTH = CELL_WIDTH * N_HORZ
+const bgcol = Color("e0e0e0")
+
+func _ready():
+	pass # Replace with function body.
+
+func _draw():
+	draw_rect(Rect2(0, 0, BOX_WIDTH, BOX_WIDTH), bgcol)
+	draw_rect(Rect2(BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH), bgcol)
+	draw_rect(Rect2(BOX_WIDTH*2, 0, BOX_WIDTH, BOX_WIDTH), bgcol)
+	draw_rect(Rect2(0, BOX_WIDTH*2, BOX_WIDTH, BOX_WIDTH), bgcol)
+	draw_rect(Rect2(BOX_WIDTH*2, BOX_WIDTH*2, BOX_WIDTH, BOX_WIDTH), bgcol)
+	pass
+
---
+++ NumberPlace/Grid.gd
@@ -0,0 +1,16 @@
+extends ColorRect
+
+const N_VERT = 9
+const N_HORZ = 9
+const CELL_WIDTH = 53
+const BOARD_WIDTH = CELL_WIDTH * N_HORZ
+
+func _ready():
+	pass # Replace with function body.
+
+func _draw():
+	for x in range(N_HORZ+1):
+		var wd = 3 if (x % 3) == 0 else 1
+		draw_line(Vector2(x*CELL_WIDTH, 0), Vector2(x*CELL_WIDTH, BOARD_WIDTH), Color.black, wd)
+		draw_line(Vector2(0, x*CELL_WIDTH), Vector2(BOARD_WIDTH, x*CELL_WIDTH), Color.black, wd)
+	pass
