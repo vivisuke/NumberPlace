a238c84d40cf1d7a36d5d2c37642c733afd9ee1f
vivisuke <ntsuda@master.email.ne.jp>
2021-12-31 11:29:15 +0900

    0340 ◎ C 12/31 12/31 0.001│問題│ヒントレイヤー：次ページ ボタンが効かない 0341 ◎ C 12/31 12/31 0.001│ToDo│ヒントレイヤー：ドットボタン押下対応

--- NumberPlace/scripts/MainScene.gd
+++ NumberPlace/scripts/MainScene.gd
@@ -1354,11 +1354,13 @@ func hint_nakid_single():
 	g.hint_bit = ns[IX_BIT]
 	return true
 func update_hint_dots():
+	#print("hint_ix = ", hint_ix)
 	$HintLayer/DotButton0.pressed = hint_ix == 0
 	$HintLayer/DotButton1.pressed = hint_ix == 2
 	$HintLayer/DotButton2.pressed = hint_ix == 4
 	if hint_texts.size()/2 > 3:
 		$HintLayer/DotButton3.pressed = hint_ix == 6
+	#print("hint_ix = ", hint_ix)
 func show_hint():
 	hint_showed = true
 	hint_ix = 0
@@ -1373,15 +1375,24 @@ func show_hint():
 	$HintLayer/NextHintButton.disabled = hint_ix == hint_texts.size() - 2
 	g.show_hint_guide = hint_texts[hint_ix + 1]
 	$Board/HintGuide.update()
-func hint_prev_next_page(d):
-	hint_ix += d * 2
-	update_hint_dots()
-	$HintLayer/Label.text = hint_texts[hint_ix].replace("%d", hint_numstr)
-	$HintLayer/PageLabel.text = "%d/%d" % [(hint_ix/2+1), (hint_texts.size()/2)]
+func update_hint_prev_next():
 	$HintLayer/PrevHintButton.disabled = hint_ix == 0
 	$HintLayer/NextHintButton.disabled = hint_ix == hint_texts.size() - 2
-	g.show_hint_guide = hint_texts[hint_ix + 1]
-	$Board/HintGuide.update()
+func hint_prev_next_page(d):
+	if !in_button_pressed:
+		in_button_pressed = true
+		hint_ix += d * 2
+		#print("hint_ix = ", hint_ix)
+		update_hint_dots()
+		#print("hint_ix = ", hint_ix)
+		$HintLayer/Label.text = hint_texts[hint_ix].replace("%d", hint_numstr)
+		$HintLayer/PageLabel.text = "%d/%d" % [(hint_ix/2+1), (hint_texts.size()/2)]
+		update_hint_prev_next()
+		#print("hint_ix = ", hint_ix)
+		g.show_hint_guide = hint_texts[hint_ix + 1]
+		$Board/HintGuide.update()
+		#print("hint_ix = ", hint_ix)
+		in_button_pressed = false
 func is_no_mistake():		# 間違って入っている数字が無いか？
 	for ix in range(N_CELLS):
 		var n = get_cell_numer(ix)
@@ -1569,3 +1580,19 @@ func _on_TweetButton_pressed():
 	var txt = "%23SakuSakuNumberPlace"
 	OS.shell_open("https://twitter.com/intent/tweet?text=" + txt)
 	pass # Replace with function body.
+
+func on_dotButtaonPressed(ix):
+	if !in_button_pressed:
+		in_button_pressed = true
+		hint_ix = ix * 2
+		update_hint_dots()
+		update_hint_prev_next()
+		in_button_pressed = false
+func _on_DotButton0_toggled(button_pressed):
+	on_dotButtaonPressed(0)
+func _on_DotButton1_toggled(button_pressed):
+	on_dotButtaonPressed(1)
+func _on_DotButton2_toggled(button_pressed):
+	on_dotButtaonPressed(2)
+func _on_DotButton3_toggled(button_pressed):
+	on_dotButtaonPressed(3)
